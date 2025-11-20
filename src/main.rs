0b9b7d1ba5ca8ccedf4c98112642020b3cb4717d
eleanor <me@eleanorkelley.com>
2025-11-20 13:08:58 -0700

    Move formatting code to separate module

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+mod format;
+
 use clap::{Arg, command, error::ErrorKind};
 use std::{
     fs::write,
@@ -76,7 +78,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut buffer = String::new();
 
-    format(&mut buffer, &ledger)?;
+    format::format(&mut buffer, &ledger)?;
 
     // Write output
     match matches.get_one::<String>("output") {
@@ -92,155 +94,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     Ok(())
-}
-
-fn format(buffer: &mut dyn std::fmt::Write, ledger: &str) -> Result<(), std::fmt::Error> {
-    let mut max_acct_len = 0;
-    let mut max_line_len = 0;
-    for line in ledger.lines() {
-        let tokens = tokenize(line);
-
-        if tokens.len() == 1 && tokens[0].chars().next().unwrap().is_ascii_digit() {
-            // TODO: Date processing
-            max_line_len = max_line_len.max(split_comments(line).0.chars().count());
-        } else if tokens.len() == 2 {
-            // crude, but assume split
-            // the + 2/4 accounts for the additional spaces added by format_account()
-            max_acct_len = max_acct_len
-                .max(tokens[0].chars().count() + if has_status(&tokens[0]) { 2 } else { 4 });
-            // + 2 spaces between account and amount
-            max_line_len =
-                max_line_len.max(max_acct_len + format_amount(&tokens[1]).chars().count() + 2);
-        } else {
-            max_line_len = max_line_len.max(split_comments(line).0.chars().count());
-        }
-    }
-
-    // write cycle
-    for line in ledger.lines() {
-        if line.trim_start().starts_with(";") {
-            writeln!(buffer, "{}", line.trim_end())?;
-            continue;
-        }
-
-        let tokens = tokenize(line);
-
-        writeln!(
-            buffer,
-            "{}",
-            format!(
-                "{:max_line_len$}{}",
-                match tokens.len() {
-                    2 => format!(
-                        "{:max_acct_len$}  {}",
-                        format_account(&tokens[0]),
-                        format_amount(&tokens[1]),
-                    ),
-                    _ => split_comments(line).0,
-                },
-                split_comments(line).1
-            )
-            .trim_end()
-        )?;
-    }
-    Ok(())
-}
-
-fn tokenize(line: &str) -> Vec<String> {
-    split_comments(line)
-        .0
-        .split("  ")
-        .map(|x| x.trim().to_string())
-        .filter(|x| !x.is_empty())
-        .collect()
-}
-
-/// Splits a line into its data and comments components.
-///
-/// Return value is a `String` tuple of the form `(data, comments)`.
-fn split_comments(line: &str) -> (String, String) {
-    if line.trim_start().starts_with(";") {
-        return (String::from(""), line.trim_end().to_string());
-    }
-
-    match line.split_once(";") {
-        None => (line.trim_end().to_string(), String::from("")),
-        Some((data, comments)) => (
-            data.trim_end().to_string(),
-            String::from(" ;") + comments.trim_end(),
-        ),
-    }
-}
-
-/// This does not determine if the amount is a valid number.
-/// It only assesses whether it is comprised of chars that compose a number.
-fn format_amount(token: &str) -> String {
-    let currency_prefix = !is_number_component(token.chars().next().unwrap());
-    let mut number = String::new();
-    let mut currency = String::new();
-
-    let mut is_number = false;
-    let mut is_currency = false;
-
-    // this loop could probably be refactored
-    for char in token.chars() {
-        if is_number {
-            if is_number_component(char) {
-                number.push(char);
-                continue;
-            } else {
-                is_number = false;
-            }
-        } else if is_currency {
-            if char == ' ' || is_number_component(char) {
-                is_currency = false;
-            } else {
-                currency.push(char);
-                continue;
-            }
-        }
-
-        // if we're not building a currency or number, start building
-        if is_number_component(char) {
-            assert!(number.is_empty());
-            is_number = true;
-            number.push(char);
-        } else if char != ' ' {
-            // is not part of a number; should be currency
-            assert!(currency.is_empty());
-            is_currency = true;
-            currency.push(char);
-        }
-    }
-
-    {
-        if currency.is_empty() {
-            number.to_string()
-        } else if currency_prefix {
-            match number.chars().next().unwrap() {
-                '-' => format!("{currency}{number}"),
-                _ => format!("{currency} {number}"),
-            }
-        } else {
-            format!("{number} {currency}")
-        }
-    }
-    .trim_end()
-    .to_string()
-}
-
-fn format_account(account: &str) -> String {
-    if has_status(account) { "  " } else { "    " }.to_string() + account.trim()
-}
-
-fn is_number_component(char: char) -> bool {
-    char.is_ascii_digit() || char == '-' || char == '.' || char == ','
-}
-
-fn has_status(token: &str) -> bool {
-    let mut chars = token.chars();
-    match chars.next().unwrap() {
-        '!' | '*' => chars.next().unwrap().is_ascii_whitespace(),
-        _ => false,
-    }
 }
